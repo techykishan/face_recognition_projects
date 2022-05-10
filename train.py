64e89da07016756ecc34dba918cd9a287b7178fe
Kishan <Kishantiwari8283877183q@gmail.com>
2022-05-10 13:06:43 +0530

    first commit

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import cv2
 
 
 class Train:
-    def __init__(self, root):
+    def __init__(self, root): 
         self.root = root
         self.root.geometry("1530x790+0+0")
         self.root.title("Face Recognition System ")
