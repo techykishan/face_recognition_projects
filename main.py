5a29e394b003f58ff4b3ec851c98d5dd0fc58fb6
kishan <kishantiwari8283877183q@gmail.com>
2022-06-18 17:52:23 +0530

    last

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from face_recognition import Face_Recognition
 from attendance import Attendance
 
 class Face_recognition_system:
-    def __init__(self, root): 
-        self.root = root 
+    def __init__(self, root):
+        self.root = root
         self.root.geometry("1530x790+0+0")
         self.root.title("Face Recognition System")
 
